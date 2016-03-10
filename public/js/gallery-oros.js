16380b3a64ad1b15f5a337bce0cecd0d5915f4d9
Philip A. Hirt <phirt13@gmail.com>
2016-03-10 03:05:11 -0600

    worked on mobile arts and added improved js

---
+++ public/js/gallery-oros.js
@@ -0,0 +1,81 @@
+$(function() {
+  var $mainImage = $('#main-image');
+  var $thumbnailOne = $('#thumbnail-0');
+  var $thumbnailTwo = $('#thumbnail-1');
+  var $thumbnailThree = $('#thumbnail-2');
+  var $thumbnailFour = $('#thumbnail-3');
+  var $thumbnailFive = $('#thumbnail-4');
+  var $thumbnailSix = $('#thumbnail-5');
+  var $thumbnailSeven = $('#thumbnail-6');
+  var $thumbnailEight = $('#thumbnail-7');
+  var $thumbnailNine = $('#thumbnail-8');
+
+  var pictures = [
+    'pics/oros/regular/oros-1.jpg',
+    'pics/oros/regular/oros-2.jpg',
+    'pics/oros/regular/oros-3.jpg',
+    'pics/oros/regular/oros-4.jpg',
+    'pics/oros/regular/oros-5.jpg',
+    'pics/oros/regular/oros-6.jpg',
+    'pics/oros/regular/oros-7.jpg',
+    'pics/oros/regular/oros-8.jpg',
+    'pics/oros/regular/oros-9.jpg'
+  ];
+
+  var counter = 0;
+
+  $thumbnailOne.click(function() {
+    $mainImage.attr('src', pictures[0]);
+    counter = 0;
+  });
+
+  $thumbnailTwo.click(function() {
+    $mainImage.attr('src', pictures[1]);
+    counter = 1;
+  });
+
+  $thumbnailThree.click(function() {
+    $mainImage.attr('src', pictures[2]);
+    counter = 2;
+  });
+
+  $thumbnailFour.click(function() {
+    $mainImage.attr('src', pictures[3]);
+    counter = 3;
+  });
+
+  $thumbnailFive.click(function() {
+    $mainImage.attr('src', pictures[4]);
+    counter = 4;
+  });
+
+  $thumbnailSix.click(function() {
+    $mainImage.attr('src', pictures[5]);
+    counter = 5;
+  });
+
+  $thumbnailSeven.click(function() {
+    $mainImage.attr('src', pictures[6]);
+    counter = 6;
+  });
+
+  $thumbnailEight.click(function() {
+    $mainImage.attr('src', pictures[7]);
+    counter = 7;
+  });
+
+  $thumbnailNine.click(function() {
+    $mainImage.attr('src', pictures[8]);
+    counter = 8;
+  });
+
+  $mainImage.click(function() {
+    if(counter >= 8) {
+      counter = 0;
+      $mainImage.attr('src', pictures[counter]);
+    } else {
+      counter++ ;
+      $mainImage.attr('src', pictures[counter]);
+    }
+  });
+});
---
+++ public/js/mobile-navbar-gallery.js
@@ -0,0 +1,26 @@
+$(function() {
+
+  var $menuIcon = $('#mobile-gallery-menu');
+  var $artNavbar = $('#artist-navbar');
+
+  var toggleLight = 'green';
+
+  $menuIcon.click(function() {
+    if (toggleLight === 'green') {
+      $artNavbar.attr('class', 'gallery-piece--navbar--active');
+      $menuIcon.attr('class', 'icon-cross2');
+
+      toggleLight = 'red';
+
+    } else if (toggleLight === 'red') {
+      $artNavbar.attr('class', 'gallery-piece--navbar');
+      $menuIcon.attr('class', 'icon-menu2');
+
+      toggleLight = 'green';
+
+    } else {
+      console.log('Not Working');
+    }
+  });
+
+});
