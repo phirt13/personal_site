35b3b7794fdafbbce64b87b284ea052d6a97798d
Philip A. Hirt <phirt13@gmail.com>
2016-03-19 16:02:01 -0500

    finished sketch gallery

---
+++ public/js/gallery-aluminum.js
@@ -0,0 +1,46 @@
+$(function() {
+  var $mainImage = $('#main-image');
+  var $thumbnailOne = $('#thumbnail-0');
+  var $thumbnailTwo = $('#thumbnail-1');
+  var $thumbnailThree = $('#thumbnail-2');
+  var $thumbnailFour = $('#thumbnail-3');
+
+  var pictures = [
+    'pics/art/sketches/aluminum/regular/aluminum-1.jpg',
+    'pics/art/sketches/aluminum/regular/aluminum-2.jpg',
+    'pics/art/sketches/aluminum/regular/aluminum-3.jpg',
+    'pics/art/sketches/aluminum/regular/aluminum-4.jpg'
+  ];
+
+  var counter = 0;
+
+  $thumbnailOne.click(function() {
+    $mainImage.attr('src', pictures[0]);
+    counter = 0;
+  });
+
+  $thumbnailTwo.click(function() {
+    $mainImage.attr('src', pictures[1]);
+    counter = 1;
+  });
+
+  $thumbnailThree.click(function() {
+    $mainImage.attr('src', pictures[2]);
+    counter = 2;
+  });
+
+  $thumbnailFour.click(function() {
+    $mainImage.attr('src', pictures[3]);
+    counter = 3;
+  });
+
+  $mainImage.click(function() {
+    if(counter >= 3) {
+      counter = 0;
+      $mainImage.attr('src', pictures[counter]);
+    } else {
+      counter++ ;
+      $mainImage.attr('src', pictures[counter]);
+    }
+  });
+});
